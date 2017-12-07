56bd85d00f847f282daa4ccc2192f9128a8e6f3b
Yu-Liang Chou <yu-liangchou@yu-liangs-macbook-air.local>
2017-12-07 14:29:06 +0800

    Initial Commit

---
+++ 1207new2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1207new2
+//
+//  Created by Yu-Liang Chou  on 2017/12/7.
+//  Copyright © 2017年 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
