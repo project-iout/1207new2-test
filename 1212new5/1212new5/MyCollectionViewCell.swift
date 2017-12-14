e2baeb9a5d93c475018be32df7b530976e351dc9
Yu-Liang Chou <yu-liangchou@yu-liangs-macbook-air.local>
2017-12-14 13:04:26 +0800

    second upload

---
+++ 1212new5/1212new5/MyCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MyCollectionViewCell.swift
+//  1212new5
+//
+//  Created by Yu-Liang Chou  on 2017/12/12.
+//  Copyright © 2017年 aa. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var MyPhoto: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
